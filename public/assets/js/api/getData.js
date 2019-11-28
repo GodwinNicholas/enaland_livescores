5cb4003a22ac9933661b66bedcc728257bf24360
Godwin Nicholas <iamgodwin@gmail.com>
2019-11-28 19:22:34 +0100

    new changes

--- public/assets/js/api/getData.js
+++ public/assets/js/api/getData.js
@@ -2,7 +2,7 @@ import render from './render.js';
 // import update from './update';
 
 // const api_uri = `http://localhost:8000/api/livescores`
-const api_uri = `http://livescores.enaland.com/api/livescores`
+const api_uri = `http://192.168.43.93:8000/api/livescores`
 
 function init() {
     const res = fetch(api_uri)
--- routes/api/livescoreApi.js
+++ routes/api/livescoreApi.js
@@ -6,16 +6,16 @@ const API_KEY = require("../../keys/api").football_api;
 // home route
 router.get("/", async (req, res) => {
     // API CALL
-    const url = `https://allsportsapi.com/api/football/?met=Countries&APIkey=${API_KEY}`;
-    const url2 = `https://allsportsapi.com/api/football/?met=Livescore&APIkey=${API_KEY}`;
-    const url3 = `https://allsportsapi.com/api/football/?met=Fixtures&APIkey=${API_KEY}&from=2019-11-28&to=2019-11-28`;
+    // const url = `https://allsportsapi.com/api/football/?met=Countries&APIkey=${API_KEY}`;
+    // const url2 = `https://allsportsapi.com/api/football/?met=Livescore&APIkey=${API_KEY}`;
+    const url3 = `https://allsportsapi.com/api/football/?met=Fixtures&APIkey=${API_KEY}&from=${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}&to=${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`;
     rp.get(url3)
         .then(async response => {
             let unsorted = JSON.parse(response).result
             // compare func
             function compare(a, b) {
-                m1 = new Date(`28 November 2019 ${a.event_time}:0`);
-                m2 = new Date(`28 November 2019 ${b.event_time}:0`);
+                m1 = new Date(`${new Date().toDateString()} ${a.event_time}:0`);
+                m2 = new Date(`${new Date().toDateString()} ${b.event_time}:0`);
                 if (m1 < m2) {
                     return -1;
                 }

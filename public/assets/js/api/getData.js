340294cffe58956a9aa6cabdd54aa76b5bc6aee0
Godwin Nicholas <iamgodwin@gmail.com>
2019-12-10 13:56:48 +0100

    colors changes

--- public/assets/js/api/getData.js
+++ public/assets/js/api/getData.js
@@ -2,9 +2,10 @@ import render from './render.js';
 // import update from './update';
 
 
-const url = window.location.href == "https://livescores.enaland.com/" ? "https://livescores.enaland.com/" : `http://192.168.43.93:8000/api/livescores`;
+const live_url = "https://livescores.enaland.com/";
+const local_url = "https://livescores.enaland.com/";
 
-const api_uri = url;
+const api_uri = live_url;
 // const api_uri = `https://livescores.enaland.com/api/livescores`
 
 function init() {

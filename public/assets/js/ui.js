4cbd429b1d2872c789afb48323da95e155999c65
Godwin Nicholas <iamgodwin@gmail.com>
2019-12-17 13:23:43 +0100

    search feature

--- public/assets/js/ui.js
+++ public/assets/js/ui.js
@@ -1,6 +1,13 @@
+const find = document.querySelector(".search #find");
+
 function openLink() {
     this.addEventListener('click', (e) => {
         e.preventDefault();
         window.open(e.target.href)
     });
 }
+
+find.addEventListener("click", async e => {
+    const word = document.querySelector("#searchTeam").value;
+    found = window.find(word) ? "" : alert("Not found!");
+});
